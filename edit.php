f6f05bb16ae969083bdf781633f85caff17e591e
Elvin Pangadongan <epangadongan@gmail.com>
2023-10-30 13:53:49 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,40 +1,93 @@
 <?php
 include("connection.php");
 if(!isset($_GET['id'])){
-	header('Location: user-table.php');
+    header('Location: tables.php');
 }
 $id=$_GET['id'];
 $sql="SELECT * FROM user WHERE id_user=$id";
 $query=mysqli_query($db, $sql);
 $user=mysqli_fetch_assoc($query);
-
 if(mysqli_num_rows($query)<1){
-	die("data tidak ditemukan...");
+    die("data tidak ditemukan...");
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-	<title>Halaman Edit</title>
-	<meta charset="utf-8">
-  	<meta content="width=device-width, initial-scale=1.0" name="viewport">
-  	<link rel="stylesheet" type="text/css" href="style.css">
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Halaman Edit</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.css" rel="stylesheet">
+
 </head>
-<body>
-<div class="login">
-<h1>Edit</h1>
-<form action="edit-process.php" method="POST">
 
-<input type="hidden" name="id" value="<?php echo $user['id_user']?>">
+<body class="bg-gradient-primary">
 
-<label>Full Name</label><br>
-<input type="text" name="name" value="<?php echo $user['name']?>"><br>
-<label>Username</label><br>
-<input type="text" name="username" value="<?php echo $user['username']?>"><br>
-<label>Password</label><br>
-<input type="password" name="password" value="<?php echo $user['password']?>"><br><br>
-<input type="submit" value="Save" name="save">
-</form>
-</div>
+    <div class="container">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                    <div class="col-lg-7">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Perbarui Data User!</h1>
+                            </div>
+                            <form class="user" action="edit-process.php" method="POST">
+                                <input type="hidden" name="id" value="<?php echo $user['id_user']?>">
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" id="exampleFirstName" name="name" value="<?php echo $user['name']?>">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user" id="exampleInputEmail"name="username" value="<?php echo $user['username']?>">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-user"
+                                    id="exampleInputPassword" name="password" value="<?php echo $user['password']?>">
+                                </div>
+                                <input type="submit" value="Simpan" name="save" class="btn btn-primary btn-user btn-block">
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                <a class="small" href="forgot-password.html">Lupa Password?</a>
+                            </div>
+                            <div class="text-center">
+                                <a class="small" href="index.php">Sudah Punya Akun? Silakan Login!</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
 </body>
+
 </html>
